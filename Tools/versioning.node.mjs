e5cc759d34ecdfd5e1c3f7960e0a070eafe78ebc
snomiao <snomiao@gmail.com>
2021-03-18 05:42:20 +0800

    fix:versioning

--- Tools/versioning.node.mjs
+++ Tools/versioning.node.mjs
@@ -9,6 +9,5 @@ const choco包文 = await readFile(choco包文件, 'utf-8')
 const 新版本包文 = choco包文.replace(/(<version>)(.*?)(<\/version>)/, (_, $1, $2, $3) => $1 + version + $3)
 console.assert(choco包文 !== 新版本包文, `警告：Choco包版本没有变化，当前版本为${version}`)
 await writeFile(choco包文件, 新版本包文)
-await promisify(exec)('git commit -a -m "versioning"')
 
 // await promisify(exec)('start "" tools/pack.bat')
