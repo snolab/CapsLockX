c8c40a84071c63bd2f7f94da5852b3cc51db79a5
snomiao <snomiao@gmail.com>
2021-04-11 21:58:53 +0800

    fix: 升级git push方式：合并remote

---
+++ Tools/setup_repos.bat
@@ -0,0 +1,11 @@
+@echo off
+chcp 65001
+REM [Working with Git remotes and pushing to multiple Git repositories | Jigarius.com]( https://jigarius.com/blog/multiple-git-remote-repositories )
+git remote remove remote
+git remote add remote git@github.com:snomiao/CapsLockX.git
+git remote set-url --add --push remote git@github.com:snomiao/CapsLockX.git
+git remote set-url --add --push remote git@gitee.com:snomiao/CapslockX.git
+git remote set-url --add --push remote git@bitbucket.org:snomiao/capslockx.git
+git fetch --all 
+git push --set-upstream remote master
+git push
