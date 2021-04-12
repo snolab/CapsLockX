a2eed8efd3539a40e2b30bbe6fb8d7e997598cfe
snomiao <snomiao@gmail.com>
2021-04-12 13:30:41 +0800

    ignore tgz

--- Tools/push.bat
+++
@@ -1,3 +0,0 @@
-git push --repo git@github.com:snomiao/CapsLockX.git && git push --follow-tag --repo git@github.com:snomiao/CapsLockX.git
-git push --repo git@gitee.com:snomiao/CapslockX.git && git push --follow-tag --repo git@gitee.com:snomiao/CapslockX.git
-git push --repo git@bitbucket.org:snomiao/capslockx.git && git push --follow-tag --repo git@bitbucket.org:snomiao/capslockx.git
