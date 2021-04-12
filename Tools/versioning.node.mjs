c2586f6030ccfd5b4a7a41c3b1a7d85399955913
snomiao <snomiao@gmail.com>
2021-04-12 12:21:52 +0800

    chore: versioning 过长文档 CDATA包装

--- Tools/versioning.node.mjs
+++ Tools/versioning.node.mjs
@@ -1,5 +1,5 @@
 import { readFile, writeFile } from 'fs/promises'
-const 文本读 = (path) => readFile(path, 'utf-8')
+const 文本读 = (path) => readFile(path, 'utf-8') || ''
 const { version } = JSON.parse(await 文本读('package.json'))
 
 // 更新版本号txt
@@ -11,8 +11,9 @@ await writeFile(txt版本号路径, version)
 // 更新choco包文件
 const choco包文件路径 = 'Tools/choco/CapsLockX.nuspec'
 const choco包文 = await 文本读(choco包文件路径)
-const README = '<![CDATA[' + await 文本读('README.MD') + ']]>'
-const CHANGELOG = '<![CDATA[' + await 文本读('CHANGELOG.MD') + ']]>'
+const CDATA包装 = 文本 => `<![CDATA[${文本.slice(0, 3888).split(/\r?\n/g).slice(0, -1).join('\n')}]]>`
+const README = CDATA包装(await 文本读('README.MD'))
+const CHANGELOG = CDATA包装(await 文本读('CHANGELOG.MD'))
 const 新版本包文 = choco包文
     .replace(/(<version>)(.*?)(<\/version>)/, (_, $1, $2, $3) => $1 + version + $3)
     .replace(/(<description>)([\s\S]*?)(<\/description>)/, (_, $1, $2, $3) => $1 + README + $3)
