d693ddfecba83fc65ac16ed82590cb32f305404e
snomiao <snomiao@gmail.com>
2021-04-13 08:29:03 +0800

    chore: remove snomiao/CapsLockX.git

--- Tools/setup_repos.bat
+++ Tools/setup_repos.bat
@@ -3,7 +3,6 @@ chcp 65001
 REM [Working with Git remotes and pushing to multiple Git repositories | Jigarius.com]( https://jigarius.com/blog/multiple-git-remote-repositories )
 git remote remove remote
 git remote add remote git@github.com:snomiao/CapsLockX.git
-git remote set-url --add --push remote git@github.com:snomiao/CapsLockX.git
 git remote set-url --add --push remote git@github.com:snolab/CapsLockX.git
 git remote set-url --add --push remote git@gitlab.com:snomiao/CapsLockX.git
 git remote set-url --add --push remote git@gitee.com:snomiao/CapslockX.git
