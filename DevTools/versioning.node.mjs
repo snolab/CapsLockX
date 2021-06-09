b7f9d3e3c6952f49081a9c8e840fc283852b6961
snomiao <snomiao@gmail.com>
2021-06-09 11:23:21 +0800

    chore: update english description

--- DevTools/versioning.node.mjs
+++ DevTools/versioning.node.mjs
@@ -12,11 +12,11 @@ await writeFile(txt版本号路径, version)
 const choco包文件路径 = './DevTools/choco/CapsLockX.nuspec'
 const choco包文 = await 文本读(choco包文件路径)
 const CDATA包装 = 文本 => `<![CDATA[${文本.slice(0, 3888).split(/\r?\n/g).slice(0, -1).join('\n')}]]>`
-const README = CDATA包装(await 文本读('README.MD'))
-const CHANGELOG = CDATA包装(await 文本读('CHANGELOG.MD'))
+const README = CDATA包装(await 文本读('README.MD')).replace(/[一-龥]+/g, '')
+const CHANGELOG = CDATA包装(await 文本读('CHANGELOG.MD')).replace(/[一-龥]+/g, '')
 const 新版本包文 = choco包文
     .replace(/(<version>)(.*?)(<\/version>)/, (_, $1, $2, $3) => $1 + version + $3)
-    .replace(/(<description>)([\s\S]*?)(<\/description>)/, (_, $1, $2, $3) => $1 + README + $3)
+    // .replace(/(<description>)([\s\S]*?)(<\/description>)/, (_, $1, $2, $3) => $1 + README + $3)
     .replace(/(<releaseNotes>)([\s\S]*?)(<\/releaseNotes>)/, (_, $1, $2, $3) => $1 + CHANGELOG + $3)
 console.assert(choco包文 !== 新版本包文, `警告：Choco包文版本没有变化，当前版本为${version}`)
 await writeFile(choco包文件路径, 新版本包文)
