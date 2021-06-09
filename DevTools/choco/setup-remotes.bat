605dbd38e02602b66495616e28c488fb1ce1cb04
snomiao <snomiao@gmail.com>
2021-06-09 11:29:35 +0800

    chore: setup-remotes

---
+++ DevTools/choco/setup-remotes.bat
@@ -0,0 +1,5 @@
+
+git remote add all git@github.com:snolab/CapsLockX
+git remote set-url --add all git@gitee.com:snomiao/CapslockX
+git remote set-url --add all git@bitbucket.com.com:snomiao/capslockx
+git remote set-url --add all git@gitlab.com:snomiao/CapsLockX
