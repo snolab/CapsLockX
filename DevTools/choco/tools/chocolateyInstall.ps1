b7c0c1de25073e9f9fb1b9b4b03bd044afc764bf
snomiao <snomiao@gmail.com>
2021-08-17 13:59:26 +0800

    chore: choco push

--- DevTools/choco/tools/chocolateyInstall.ps1
+++ DevTools/choco/tools/chocolateyInstall.ps1
@@ -1,21 +1,8 @@
-﻿$ErrorActionPreference = 'Stop'; # stop on all errors
+﻿$ErrorActionPreference = 'Stop';
 $packageName    = 'CapsLockX'
 $toolsDir       = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
 $zipFilePath    = "$toolsDir\\CapsLockX.zip"
-# $url            = 'https://github.com/snomiao/CapsLockX/archive/master.zip'
-# $checksum       = '%checksum%'
-# $checksumType   = 'md5'
 $packageArgs = @{
   packageName   = $env:ChocolateyPackageName
-  # unzipLocation = $toolsDir
-  # url           = $url
-  # softwareName  = 'CapsLockX' #part or all of the Display Name as you see it in Programs and Features. It should be enough to be unique
-  # checksum      = '%checksum%'
-  # checksumType  = 'sha256' #default is md5, can also be sha1, sha256 or sha512
 }
 Get-ChocolateyUnzip -FileFullPath $zipFilePath -Destination $toolsDir
-# Install-ChocolateyZipPackage -PackageName "$packageName" `
-#                              -UnzipLocation "$toolsDir"
-#                              -Url "$url" `
-#                              -Checksum "$checksum" `
-#                              -ChecksumType "$checksumType"
