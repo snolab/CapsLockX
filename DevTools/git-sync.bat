ab60ceafcee98608b5e86de22f752e9d9b787edf
snomiao <snomiao@gmail.com>
2022-02-25 00:35:32 +0800

    chore: git sync script

--- DevTools/git-sync.bat
+++ DevTools/git-sync.bat
@@ -1,11 +1,13 @@
+cd %~dp0
+cd ..
 
 @REM add remotes
 git remote remove all
 git remote add all git@github.com:snolab/CapsLockX.git
 git remote set-url --add all git@github.com:snomiao/CapsLockX.git
-git remote set-url --add all git@gitee.com:snomiao/CapslockX.git
 git remote set-url --add all git@bitbucket.org:snomiao/capslockx.git
 git remote set-url --add all git@gitlab.com:snomiao/CapsLockX.git
+git remote set-url --add all git@gitee.com:snomiao/CapslockX.git
 git remote -v
 
 @REM sync
