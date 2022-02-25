84eaf5d8e4c9d2bda50c67e2b821608c2d530485
snomiao <snomiao@gmail.com>
2022-02-25 11:38:46 +0800

    chore: lints

---
+++ .eslintrc.js
@@ -0,0 +1,12 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+  },
+  extends: ['airbnb-base'],
+  parserOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+  },
+  rules: {},
+};
