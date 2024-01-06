aed34e5b2108e201597fa3315379b44195003749
snomiao <snomiao@users.noreply.github.com>
2024-01-06 10:36:02 +0000

    deploy: 7769cff3d9a323338631ae93cb1c53322152f831

--- translate.ts
+++ translate.ts
@@ -1,2 +1,10 @@
-import OpenAI from 'openai'
-new OpenAI().chat.completions.create({stream: true, model: "gpt-4-1106-preview",messages: [{role: 'user', content: '' }, {role: 'user', content: 'Translate into English:' }]})
+import OpenAI from "openai";
+import 'dotenv/config';
+new OpenAI().chat.completions.create({
+  stream: true,
+  model: "gpt-4-1106-preview",
+  messages: [
+    { role: "user", content: "以上烴為国工和採" },
+    { role: "user", content: "Translate into English:" },
+  ],
+});
